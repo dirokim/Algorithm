3f7c2b866e1ca8895940fb9ea250040a883e2096
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2024-06-22 21:23:27 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if(a!=b&&a!=c&&b!=c){
+            answer = a+b+c;
+        }else if(a==b&&b!=c){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else if(a==c&&c!=b){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else if(b==c&&a!=b){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else if(a==b&&a==c){
+            answer = (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }
+        return answer;
+    }
+}
