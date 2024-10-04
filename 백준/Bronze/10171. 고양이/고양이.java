f8fa431da4ca0638075c85155cbd2e2c660d7dcd
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2024-10-04 13:06:37 +0900

    [Bronze V] Title: 고양이, Time: 96 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,8 @@
+import java.io.*; 
+public class Main{
+	public static void main(String[]args)throws IOException{  
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n )  ( \')\n(  /  )\n \\(__)|"); 
+        System.out.println(sb.toString());
+	} 
+}
