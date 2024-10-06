b6ff89f98ee92f0174c3b37522a9a6500cac14a7
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2024-10-06 23:30:42 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main{
+	public static void solution()throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        System.out.println(""+br.readLine()+"??!");
+        br.close();
+	}
+	public static void main(String[]args) throws IOException{
+		solution();
+	}
+}
+
