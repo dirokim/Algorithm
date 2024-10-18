9204ebeeb33fa1ebf4cac94e18313867c5afc3dd
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2024-10-18 16:56:11 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,13 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{ 
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String answer = "";
+        answer += "40"+"\n";
+        answer += "dirokim7";
+        bw.write(answer); 
+        bw.flush();
+        bw.close();
+	} 
+}
