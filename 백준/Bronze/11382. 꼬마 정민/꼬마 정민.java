8458950f2fd0764e65bff3ec224d0ab6b97b2107
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2024-10-25 16:52:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 112 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,19 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input," ");
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        long num = a+b+c;
+        String answer = ""+num;
+        bw.write(answer);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
