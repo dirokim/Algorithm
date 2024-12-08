808f8d0ee4e47a47094a7d440eb3e02d0d905940
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2024-12-08 16:58:15 +0900

    [Bronze IV] Title: 모음의 개수, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,19 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        int num = 0;
+        for(int i=0;i<input.length();i++){
+            if(input.charAt(i)=='a'||input.charAt(i)=='e'||input.charAt(i)=='i'||input.charAt(i)=='o'||input.charAt(i)=='u'){
+                num++;
+            }   
+        }
+        bw.write(""+num);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
