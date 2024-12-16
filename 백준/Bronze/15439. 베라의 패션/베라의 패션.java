63bb63156f9dcecbe58971fa3cc8997100c07b9c
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2024-12-16 17:47:47 +0900

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,14 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        num = num * (num-1);
+        bw.write(""+num);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
