537b32fd2a36795d89ae7f88c13b08f0fb4b275f
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2024-12-27 19:39:51 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,16 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        while((input = br.readLine()) != null){
+            input = new StringBuilder(input).reverse().toString();
+            bw.write(input+"\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
