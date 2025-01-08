25fe150558f947f7fee342559d4a7adf6af40fb4
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-01-08 20:48:02 +0900

    [Bronze V] Title: 개, Time: 96 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,16 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        bw.write("|\\_/|"+"\n");
+        bw.write("|q p|   /}"+"\n");
+        bw.write("( 0 )\"\"\"\\"+"\n");
+        bw.write("|\"^\"`    |"+"\n");
+        bw.write("||_/=\\\\__|");
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
