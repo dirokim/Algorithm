06528920dacb68357ac740e279cf560be1ecda3e
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-03-04 23:33:23 +0900

    [Bronze V] Title: Plane, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,21 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int num = a * b;
+        int num2 = c * d;
+        int answer = num + num2;
+        bw.write(""+answer);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
