7fa4b2ff35996e1173a6701327e99eb39b3efbf7
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-04-04 14:31:02 +0900

    [Bronze V] Title: 이상한 기호, Time: 104 ms, Memory: 14240 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -4,14 +4,12 @@ public class Main{
 	public static void main(String[]args)throws IOException{
 		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
-        String input = br.readLine();
-        StringTokenizer st = new StringTokenizer(input," ");
-        int num = Integer.parseInt(st.nextToken());
-        int num2 = Integer.parseInt(st.nextToken());
-        num = (num+num2)*(num-num2);
-        input = ""+num;
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        Long num  = Long.parseLong(st.nextToken());
+        Long num2  = Long.parseLong(st.nextToken());
+        Long answer = (num+num2) * (num-num2);
+        bw.write(""+answer);
         br.close();
-        bw.write(input);
         bw.flush();
         bw.close();
 	} 
