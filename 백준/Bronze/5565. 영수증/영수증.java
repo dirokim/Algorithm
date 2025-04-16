b95104921580ca745525d5822f7f93e489585c65
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-04-16 13:10:23 +0900

    [Bronze IV] Title: 영수증, Time: 108 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = "";
+        int answer = Integer.parseInt(br.readLine());
+        while((input = br.readLine()) != null){
+            answer -= Integer.parseInt(input);
+        }
+        bw.write(""+answer);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
