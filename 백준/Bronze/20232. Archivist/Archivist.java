3356103f5f8241b39c770ddbe22c8d1fcbd6e91a
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-06-29 23:38:34 +0900

    [Bronze IV] Title: Archivist, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.java
@@ -0,0 +1,63 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        if(input.equals("1995")){
+            bw.write("ITMO");
+        }else if(input.equals("1996")){
+            bw.write("SPbSU");
+        }else if(input.equals("1997")){
+            bw.write("SPbSU");
+        }else if(input.equals("1998")){
+            bw.write("ITMO");
+        }else if(input.equals("1999")){
+            bw.write("ITMO");
+        }else if(input.equals("2000")){
+            bw.write("SPbSU");
+        }else if(input.equals("2001")){
+            bw.write("ITMO");
+        }else if(input.equals("2002")){
+            bw.write("ITMO");
+        }else if(input.equals("2003")){
+            bw.write("ITMO");
+        }else if(input.equals("2004")){
+            bw.write("ITMO");
+        }else if(input.equals("2005")){
+            bw.write("ITMO");
+        }else if(input.equals("2006")){
+            bw.write("PetrSU, ITMO");
+        }else if(input.equals("2007")){
+            bw.write("SPbSU");
+        }else if(input.equals("2008")){
+            bw.write("SPbSU");
+        }else if(input.equals("2009")){
+            bw.write("ITMO");
+        }else if(input.equals("2010")){
+            bw.write("ITMO");
+        }else if(input.equals("2011")){
+            bw.write("ITMO");
+        }else if(input.equals("2012")){
+            bw.write("ITMO");
+        }else if(input.equals("2013")){
+            bw.write("SPbSU");
+        }else if(input.equals("2014")){
+            bw.write("ITMO");
+        }else if(input.equals("2015")){
+            bw.write("ITMO");
+        }else if(input.equals("2016")){
+            bw.write("ITMO");
+        }else if(input.equals("2017")){
+            bw.write("ITMO");
+        }else if(input.equals("2018")){
+            bw.write("SPbSU");
+        }else if(input.equals("2019")){
+            bw.write("ITMO");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
