5e3e57eb9839797d85a46684a16699bd54926c15
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-08-10 14:39:25 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
@@ -0,0 +1,28 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        String answer = "";
+        for(int i=1;i<=num;i++){
+              if(i==1){
+                  answer = "*";
+              }else{
+                  answer += " *";
+              }
+        }
+        for(int i=1;i<=num;i++){
+            if(i != 1)bw.write("\n");
+            if(i % 2 == 0){
+                bw.write(" "+answer);
+            }else{
+                bw.write(answer);
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
