e9dcc4c50a39c96b6a0fc5cc75206bb598c712dd
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-08-12 23:34:38 +0900

    [Bronze III] Title: TGN, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/5063. TGN/TGN.java
@@ -0,0 +1,28 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        while((input = br.readLine()) != null){
+            StringTokenizer st = new StringTokenizer(input," ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            c = Integer.parseInt(st.nextToken());
+            if(a == (b-c)){
+                bw.write("does not matter\n");
+            }else if(a > (b-c)){
+                bw.write("do not advertise\n");
+            }else if(a < (b-c)){
+                bw.write("advertise\n");
+            }
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
