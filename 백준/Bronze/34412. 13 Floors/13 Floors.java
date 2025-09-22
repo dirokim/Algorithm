a836a16a2a21f8c4bbf8f10734bef4b8cc82180d
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-09-22 23:47:22 +0900

    [Bronze V] Title: 13 Floors, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/34412. 13 Floors/13 Floors.java
@@ -0,0 +1,16 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        if(num >= 13){
+            num++;
+        }
+        bw.write(""+num);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
