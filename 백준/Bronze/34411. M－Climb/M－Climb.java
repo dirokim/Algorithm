5f31f8e731e0710e9788942fc6da747702f6256c
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-09-24 18:15:57 +0900

    [Bronze V] Title: M-Climb, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/34411. M－Climb/M－Climb.java
@@ -0,0 +1,13 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+        bw.write(""+num);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
