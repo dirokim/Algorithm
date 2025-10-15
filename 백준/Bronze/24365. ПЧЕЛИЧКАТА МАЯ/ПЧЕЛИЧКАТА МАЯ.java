00688684c86e1011cc4a24be4d5b814f60d6819b
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-10-15 20:16:44 +0900

    [Bronze IV] Title: ПЧЕЛИЧКАТА МАЯ, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/24365. ПЧЕЛИЧКАТА МАЯ/ПЧЕЛИЧКАТА МАЯ.java
@@ -0,0 +1,16 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int num = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+        num = num2 - num + (Integer.parseInt(st.nextToken())-num2);
+        bw.write(""+num);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
