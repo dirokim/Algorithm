178a86c238712abf815feec1fc98a13f606d4907
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-10-25 16:35:18 +0900

    [Bronze IV] Title: 2, 4, 6, 8, Time: 100 ms, Memory: 14008 KB -BaekjoonHub

---
+++ 백준/Bronze/34509. 2， 4， 6， 8/2， 4， 6， 8.java
@@ -0,0 +1,12 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        bw.write("42");
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
