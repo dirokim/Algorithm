a642d89c7868d361e8221955e35b9e2ba881b046
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-11-01 20:03:45 +0900

    [Bronze III] Title: 쿠키애호가, Time: 116 ms, Memory: 15916 KB -BaekjoonHub

---
+++ 백준/Bronze/11134. 쿠키애호가/쿠키애호가.java
@@ -0,0 +1,25 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        int a = 0;
+        int b = 0;
+        int answer = 0;
+        while((input = br.readLine()) != null){
+            StringTokenizer st = new StringTokenizer(input," ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            answer = a / b;
+            if(a % b > 0){
+                answer++;
+            }
+            bw.write(""+answer+"\n");
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
