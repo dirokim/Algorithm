e250fca662436ec3c67ae3eddc4032586e1205e9
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2025-11-28 22:14:05 +0900

    [Bronze IV] Title: Area, Time: 108 ms, Memory: 14360 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/28490. Area/Area.java
@@ -0,0 +1,26 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        String input = br.readLine();
+        int a = 0;
+        int b = 0;
+        int sum = 0;
+        int answer = 0;
+        while((input = br.readLine()) != null){
+            StringTokenizer st = new StringTokenizer(input," ");
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            sum = a * b;
+            if(sum > answer){
+                answer = sum;
+            }
+        }
+        bw.write(""+answer);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
