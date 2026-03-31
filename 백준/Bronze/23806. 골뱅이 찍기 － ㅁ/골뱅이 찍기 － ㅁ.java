97a33cafa9e4145e305c0be6bb441789baf49c91
KimSeungkyun <150798175+dirokim@users.noreply.github.com>
2026-03-31 21:18:24 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 208 ms, Memory: 77748 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.java
@@ -0,0 +1,37 @@
+import java.io.*; 
+import java.util.*;
+public class Main{
+	public static void main(String[]args)throws IOException{
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        String answer = "";
+        String sum = "";
+        String sum2 = "";
+        for(int i=0;i<num;i++){
+            sum += "@@@@@";
+        }
+        for(int i=0;i<num;i++){
+            sum2 += "@";
+        }
+        for(int i=0;i<num;i++){
+            sum2 += "   ";
+        }
+        for(int i=0;i<num;i++){
+            sum2 += "@";
+        }
+        for(int i=0;i<num;i++){
+            answer += sum+"\n";
+        } 
+        for(int i=0;i<num * 3;i++){
+            answer += sum2+"\n"; 
+        } 
+        for(int i=0;i<num;i++){
+            answer += sum+"\n";
+        }
+        bw.write(answer);
+        br.close();
+        bw.flush();
+        bw.close();
+	} 
+}
